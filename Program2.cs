a2aec3dd1c0ab17872d688980914d25a9b2bb2f8
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-06-17 11:08:37 +0000

    derive rectangle class that implement the getarea method

--- Program2.cs
+++ Program2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 
 
 namespace ConsoleApp2
@@ -18,6 +19,22 @@ namespace ConsoleApp2
         public override int GetArea()
         {
             return (22 / 7) * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public int Length { get; set; }
+        public int Breadth { get; set; }
+        public Rectangle(int length, int breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public override int GetArea()
+        {
+            return Length * Breadth;
         }
     }
     class Program2
