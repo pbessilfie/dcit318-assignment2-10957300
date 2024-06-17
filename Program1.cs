d7f9aefd83fb809409eed05ca281ef6b0d0158ae
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-06-17 10:02:09 +0000

    derive Dog class which overrides the makesound method in the animal class

--- Program1.cs
+++ Program1.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program1
+{
+
+    public class Animal
+    {
+        public virtual void MakeSound() { 
+        Console.WriteLine("Roar");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound() { 
+        Console.WriteLine("Bark");
+        }
+    }   
+    static void Main()
+    {
+       
+    }
+}
