6f5c948d7f60b0e200983948945569f0a44a531a
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-06-17 10:49:35 +0000

    created an abstract class shape with an abstract method getarea

---
+++ Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    internal class Program2
+    {
+        abstract class Shape
+        {
+            public abstract int GetArea();
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
