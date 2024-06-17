31c72f4366a6111542066f8bba7119de9d66c5f9
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-06-17 10:49:50 +0000

    initial commit

--- obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf0453c35a279152a32d5161acbb62043bd10d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490b56248a1613bcd2b080db9e5eba292824fd38")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
